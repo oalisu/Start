97426bd49f8dce7ba89eddd2af8baf9d2c215ea1
Alysson Goncalves <135854288+oalisu@users.noreply.github.com>
2023-10-20 17:21:12 +0100

    test

--- Cusrin/javascript_project.js
+++ Cusrin/javascript_project.js
@@ -1,4 +1,10 @@
-var pounds = prompt("How much is your weight in pounds?")
-var kg = pounds*0.454
-var total = alert("Your weight in KG is:"+kg+"KG")
-console.log("Conversion completed")
+// var pounds = prompt("How much is your weight in pounds?")
+// var kg = pounds*0.454
+// var total = alert("Your weight in KG is:"+kg+"KG")
+// console.log("Conversion completed")
+
+
+var x = 1
+var y = 2
+
+x >= 0 || y === "2"
