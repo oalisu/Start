f022bd5bc8f13fe36f1708cd342cf5b388ce7eb4
oalisu <alysson.dg@hotmail.com>
2024-10-19 19:30:12 +0200

    First workbook

---
+++ Java/JavaStart.java
@@ -0,0 +1,11 @@
+/**
+ * JavaStart new java course projects
+ */
+public class JavaStart {
+
+    public static void main(String[] args) {
+        System.err.println("Hello World after being compiled first time");
+        System.err.println("Testing what is the output whit two commands");
+    }     
+
+}
---
+++ Java/WorkbookOne.java
@@ -0,0 +1,10 @@
+public class WorkbookOne {
+    
+    public static void main(String[] args) {
+        System.err.println("Printing is fun!");
+        System.err.println("Java > Python");
+        System.err.println("I spilled java all over my paper");
+        System.err.println("My dog ate my java");
+    }
+
+}
