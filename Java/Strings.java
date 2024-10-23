672b3247c0859164d439b6013453ee28a2100b7c
oalisu <alysson.dg@hotmail.com>
2024-10-23 20:34:47 +0200

    Strings training

---
+++ Java/Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+
+    public static void main(String[] args) {
+
+        int year = 2017;
+
+        String winner = "Gremio";
+
+        System.err.println("The winner of " + year + " Libertadores is: " + winner + "!!!");
+
+
+    }
+
+}
---
+++ Java/WorkbookTwo.java
@@ -0,0 +1,16 @@
+public class WorkbookTwo {
+
+    public static void main(String[] args) {
+        
+        int points = 0;
+
+        points -= 50;
+        points -= 3;
+        points += 30;
+        points += 100;
+        points += 60;
+
+        System.err.println(points);
+    }
+    
+}
