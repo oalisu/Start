db33af5e03edc09879a19570e031c7e18b32bf48
oalisu <alysson.dg@hotmail.com>
2024-11-09 10:31:27 +0100

    morning exc

---
+++ Java/MathOperations.java
@@ -0,0 +1,30 @@
+public class MathOperations {
+    public static void main(String[] args) {
+
+        int bagOfSweets1 = 5;
+        int bagOfSweets2 = 10;
+
+        long starsInMilkyWay = 250_000_000_000L; //250 billion
+        long starsInAndromeda = 1_000_000_000_000L; // 1 trillion
+
+        double testScore = 6.7;
+
+        System.out.println("Fred and George collected <result> sweets");
+        System.out.println("There are <result> stars in the Milky Way and Andromeda galaxies.");
+
+        System.out.println("Fred has <result> more sweets than George");
+        System.out.println("The Andromeda Galaxy has <result> more stars than the Milky Way");
+
+        System.out.println("Snape made an error on his excel, accidentally multiplying Ron's score to: <result>/10");
+        System.out.println("Snape caught the error. Furious, he divided Ron's score to: <result>");
+        
+        System.out.println("10 is an even number, since dividing 10 by 2 has a remainder of: <result>");
+        System.out.println("5 is an odd number, since dividing 5 by 2 has a remainder of: <result>");
+
+        int counter = 0;
+        System.out.println(counter);
+        System.out.println(counter);
+
+
+    }
+}
---
+++ Java/WorkbookThree.java
@@ -0,0 +1,22 @@
+public class WorkbookThree {
+    
+public static void main(String[] args) {
+    
+    String name = "Alysson";
+    int age = 25;
+    String country = "Brazil";
+    String sportOne = "Football";
+    int timePlaying = 2;
+    String sportTwo = "Chess";
+    String subject = "Chemistry";
+    char grade = 'A';
+
+    System.err.println("My name is " + name + ". I am " + age + " years old, and I am from " + country + ".");
+    System.err.println("My favorite sport is " + sportOne + ". I play for " + timePlaying + " hours a day.");
+    System.err.println("When i am tired I like to play " + sportTwo);
+    System.err.println("In school, my favorite subject was " + subject + ", I scored a " + grade);
+
+
+}
+
+}
